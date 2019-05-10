e8a6eccb04cc32d60ac437341a709cd896efa065
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-05-10 07:23:37 -0400

    Built the core of the game

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,20 +3,37 @@ package com.LickingHeights;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-    String rock,paper,scissors;
-    long comNum;
-      comNum = Math.round(2 * Math.random());
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        String userPlay;
+        String comPlay = "";
+        int comInt = (int) (Math.random()*3);
+        System.out.println(comInt);
 
-        String [] comPlay = new String[3];
-        comPlay[0] = "paper";
-        comPlay[1] = "rock";
-        comPlay[2] = "scissors";
-        System.out.println("the Computer played "+ comPlay[(int) comNum]);
+        if (comInt==0)
+            comPlay="R";
+        else if (comInt==1)
+            comPlay="S";
+        else if (comInt==2)
+            comPlay="P";
 
+        System.out.println("Enter in your play. ( R = rock, P = paper, S = scissors)");
+        userPlay = keyboard.nextLine();
+        userPlay = userPlay.toUpperCase();
+        System.out.println(userPlay);
+
+
+
+
+
+
+
+        }
 
     }
-}
+
+
+
+
 
 
