4dea22aa0fdcc952a4d6c9abbad9aac17a2b747d
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-05-14 07:41:33 -0400

    Started my "if" statements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -2,35 +2,37 @@ package com.LickingHeights;
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         String userPlay;
-        String comPlay = "";
-        int comInt = (int) (Math.random()*3);
-        System.out.println(comInt);
+        String[] comPlay = new String[3];
+        int comInt = (int) Math.round(2 * Math.random());
+        int userScore = 0, comScore = 0;
+        comPlay[0] = "R";
+        comPlay[1] = "P";
+        comPlay[2] = "S";
 
-        if (comInt==0)
-            comPlay="R";
-        else if (comInt==1)
-            comPlay="S";
-        else if (comInt==2)
-            comPlay="P";
-
-        System.out.println("Enter in your play. ( R = rock, P = paper, S = scissors)");
-        userPlay = keyboard.nextLine();
+        System.out.println(comPlay[comInt]);
+        System.out.println("Enter in your play. (R = rock, P = paper, S = scissors)");
+        userPlay = keyboard.next();
         userPlay = userPlay.toUpperCase();
-        System.out.println(userPlay);
+        System.out.println(comPlay[comInt]);
 
-
-
-
-
-
-
+        if (userPlay.equals(comPlay[comInt])) {
+            System.out.println("It's a tie");
         }
+        
+
+
 
     }
+}
+
+
+
+
 
 
 
